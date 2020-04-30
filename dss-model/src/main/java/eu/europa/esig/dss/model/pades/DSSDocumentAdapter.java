25b2fad11b67d6bda05b0f0e428de3d7a51b78ca
Bozho <bozhidar.bozhanov@gmail.com>
2020-04-30 21:52:38 +0300

    image document adapter to load data from local directory

--- dss-model/src/main/java/eu/europa/esig/dss/model/pades/DSSDocumentAdapter.java
+++ dss-model/src/main/java/eu/europa/esig/dss/model/pades/DSSDocumentAdapter.java
@@ -1,18 +1,26 @@
 package eu.europa.esig.dss.model.pades;
 
+import java.io.File;
 import java.util.Base64;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 import eu.europa.esig.dss.model.DSSDocument;
+import eu.europa.esig.dss.model.FileDocument;
 import eu.europa.esig.dss.model.InMemoryDocument;
 
 public class DSSDocumentAdapter extends XmlAdapter<String, DSSDocument> {
 
+    public static String imageDir; 
+    
     @Override
     public DSSDocument unmarshal(String v) throws Exception {
-        DSSDocument document = new InMemoryDocument(Base64.getDecoder().decode(v));
-        return document;
+        if (v.contains("file:")) {
+            String fileName = v.trim().replace("file:", "");
+            return new FileDocument(new File(imageDir, fileName));
+        } else {
+            return new InMemoryDocument(Base64.getDecoder().decode(v));
+        }
     }
 
     @Override
